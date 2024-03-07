8dc353046d317424f779d74d56addb920e17d65d
Anupama <anupamakelat73@gmail.com>
2024-03-07 20:34:58 +0530

    first commit

---
+++ lib/splash.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_car_service_app/home.dart';
+
+class MySplash extends StatefulWidget {
+  const MySplash({super.key});
+
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash>
+with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>const HomeScreen(),));
+    });
+  }
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    overlays: SystemUiOverlay.values);  
+    super.dispose();
+  }
+  
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+       backgroundColor: Color.fromARGB(255, 197, 237, 231),
+      body: Center(
+       
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Drive Buddy",style: TextStyle(fontSize: 40,fontWeight: FontWeight.w700),),
+            Image.asset('images/splash.jpeg'),
+          ],
+         
+         ),
+        
+      ),
+      
+      
+    );
+  }
+}
